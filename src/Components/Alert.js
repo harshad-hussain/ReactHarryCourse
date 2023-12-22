f5cdaeb61318ca0e72924898d8e2f264a4d1c17b
Harshad <127301596+harshad-hussain@users.noreply.github.com>
2023-12-22 13:46:28 +0530

    Update Alert.js

--- src/Components/Alert.js
+++ src/Components/Alert.js
@@ -12,7 +12,7 @@ function Alert(props) {
   <button type="button" className="close" data-dismiss="alert" aria-label="Close">
     <span aria-hidden="true">&times;</span>
   </button>
-</div>
+</div>    
   );
 }
 
